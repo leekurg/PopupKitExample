ebd2e4f0dc2c68c1e36204daae568e2b58661a59
Илья Аникин <anikin.idm@yandex.ru>
2024-08-23 11:55:32 +0300

    Initial Commit

---
+++ PopupKitExample/PopupKitExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  PopupKitExampleApp.swift
+//  PopupKitExample
+//
+//  Created by Илья Аникин on 23.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct PopupKitExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
