3711a96aa6e5a7cd6f8801fe9f202131ca348174
Илья Аникин <anikin.idm@yandex.ru>
2024-09-09 02:23:56 +0400

    examples

--- PopupKitExample/CoverTest.swift
+++ PopupKitExample/CoverTest.swift
@@ -11,21 +11,27 @@ struct CoverTest: View {
     @State private var c1 = false
     @State private var c2 = false
     @State private var c3 = false
+    @State private var c4 = false
     
     var body: some View {
         ScrollView {
-            Button("Half sheet 1") {
+            Button("Non-modal half sheet 1") {
                 c1.toggle()
             }
             .buttonStyle(.borderedProminent)
             
-            Button("Half sheet 2") {
+            Button("Modal interactive half sheet 2") {
                 c2.toggle()
             }
             .buttonStyle(.borderedProminent)
             
-            Button("Half sheet 3") {
+            Button("Modal non-interactive 3/4 sheet 3") {
                 c3.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Button("Modal interactive very high sheet 4") {
+                c4.toggle()
             }
             .buttonStyle(.borderedProminent)
             
@@ -35,15 +41,15 @@ struct CoverTest: View {
             Color.purple.frame(height: 150)
         }
         .cover(isPresented: $c1, background: .gray, modal: .none, cornerRadius: 10) {
-            Text("Half sheet 1")
+            Text("Non-modal half sheet 1")
                 .frame(height: 400)
         }
         .cover(isPresented: $c2, modal: .modal(interactivity: .interactive)) {
             VStack {
-                Text("Half sheet 2")
+                Text("Modal interactive half sheet 2")
                     .padding(50)
 
-                Button("Open C3") {
+                Button("Open sheet 3") {
                     c3.toggle()
                 }
                 .buttonStyle(.borderedProminent)
@@ -57,21 +63,33 @@ struct CoverTest: View {
             cornerRadius: 50
         ) {
             VStack {
-                Text("Half sheet 3")
+                Text("Modal non-interactive 3/4 sheet 3")
                     .padding(50)
                 
                 Button("Close") {
                     c3.toggle()
-                    print("Tappped")
                 }
                 .buttonStyle(.borderedProminent)
             }
             .frame(minHeight: 600)
+        }
+        .cover(
+            isPresented: $c4,
+            background: .indigo,
+            modal: .modal(interactivity: .interactive)
+        ) {
+            VStack(spacing: 0) {
+                Color.red.frame(height: 300)
+                Color.yellow.frame(height: 300)
+                Color.green.frame(height: 252)
+                Color.blue.frame(height: 100)
+            }
+            .frame(width: 300)
         }
     }
 }
 
 #Preview {
     CoverTest()
-        .debugPreviewCoverEnv(ignoresSafeAreaEdges: [.bottom])
+        .debugPreviewCoverEnv(ignoresSafeAreaEdges: .all)
 }
--- PopupKitExample/FullscreenTest.swift
+++ PopupKitExample/FullscreenTest.swift
@@ -9,51 +9,74 @@ import PopupKit
 import SwiftUI
 
 struct FullscreenTest: View {
+    @State private var f1 = false
+
     var body: some View {
-        ViewA(deep: 0)
+        ZStack {
+            Color.red.opacity(0.3)
+            
+            Button("Full 1") {
+                f1.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .fullscreen(
+            isPresented: $f1,
+            background: .ultraThinMaterial,
+            ignoresEdges: [.bottom, .leading],
+            dismissalScroll: .none
+        ) {
+            ViewA(deep: 0)
+        }
     }
 }
 
 fileprivate struct ViewA: View {
     @State var deep: Int
     @State var f1 = false
+    @State var fc = false
     
     @EnvironmentObject var fullscreenPresenter: FullscreenPresenter
 
     var body: some View {
-        ZStack {
-            Rectangle()
-                .fill(.ultraThinMaterial)
+        VStack {
+            Text("Fullscreen \(deep)")
+                .font(.title)
             
-            VStack {
-                Text("Fullscreen \(deep)")
-                    .font(.title)
-                
-                HStack {
-                    Button("Next fullscreen") {
-                        f1.toggle()
-                    }
+            Spacer()
+
+            HStack {
+                Button("Next fullscreen") {
+                    f1.toggle()
                 }
-                .buttonStyle(.borderedProminent)
-                
-                Button("Pop to root") {
-                    fullscreenPresenter.popToRoot()
-                }
-                .buttonStyle(.bordered)
-                
-                Button("Pop last") {
-                    fullscreenPresenter.popLast()
-                }
-                .buttonStyle(.bordered)
-                
-//                Button("") {
-//                    fullscreenPresenter.popLast()
-//                }
-//                .buttonStyle(.bordered)
             }
-            .fullscreen(isPresented: $f1) {
-                ViewA(deep: deep + 1)
+            .buttonStyle(.borderedProminent)
+            
+            Button("Pop to root") {
+                fullscreenPresenter.popToRoot()
             }
+            .buttonStyle(.bordered)
+            
+            Button("Pop last") {
+                fullscreenPresenter.popLast()
+            }
+            .buttonStyle(.bordered)
+            
+            Button("Real fullscreenCover") {
+                fc.toggle()
+            }
+            .buttonStyle(.borderedProminent)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .border(.green)
+        .fullscreen(isPresented: $f1) {
+            ViewA(deep: deep + 1)
+        }
+        .fullScreenCover(isPresented: $fc) {
+            Text("Real fullscreen cover")
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .border(.red)
+                .ignoresSafeArea(.all, edges: .top)
         }
     }
 }
